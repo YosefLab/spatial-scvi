24f486dc8c26e54128d4581cd64c41edd5a8dc00
LevyNat <levynath5@gmail.com>
2023-07-11 08:08:38 -0700

    add compo/niche specific model/module

---
+++ scvi/model/_nichescvi.py
@@ -0,0 +1,427 @@
+import logging
+from typing import List, Literal, Optional, Sequence
+
+import numpy as np
+from anndata import AnnData
+import torch
+import torch.nn.functional as F
+
+from sklearn.neighbors import NearestNeighbors
+
+
+from scvi import REGISTRY_KEYS
+from scvi._types import MinifiedDataType
+from scvi.data import AnnDataManager
+from scvi.data._constants import _ADATA_MINIFY_TYPE_UNS_KEY, ADATA_MINIFY_TYPE
+from scvi.data._utils import _get_adata_minify_type
+from scvi.data.fields import (
+    BaseAnnDataField,
+    CategoricalJointObsField,
+    CategoricalObsField,
+    LayerField,
+    NumericalJointObsField,
+    NumericalObsField,
+    ObsmField,
+    StringUnsField,
+)
+from scvi.model._utils import _init_library_size
+from scvi.model.base import UnsupervisedTrainingMixin
+from scvi.model.utils import get_minified_adata_scrna
+from scvi.module import nicheVAE
+from scvi.utils import setup_anndata_dsp
+
+from .base import ArchesMixin, BaseMinifiedModeModelClass, RNASeqMixin, VAEMixin
+
+_SCVI_LATENT_QZM = "_scvi_latent_qzm"
+_SCVI_LATENT_QZV = "_scvi_latent_qzv"
+_SCVI_OBSERVED_LIB_SIZE = "_scvi_observed_lib_size"
+
+logger = logging.getLogger(__name__)
+
+
+class nicheSCVI(
+    RNASeqMixin,
+    VAEMixin,
+    ArchesMixin,
+    UnsupervisedTrainingMixin,
+    BaseMinifiedModeModelClass,
+):
+    """single-cell Variational Inference :cite:p:`Lopez18`.
+
+    Parameters
+    ----------
+    adata
+        AnnData object that has been registered via :meth:`~scvi.model.SCVI.setup_anndata`.
+    n_hidden
+        Number of nodes per hidden layer.
+    n_latent
+        Dimensionality of the latent space.
+    n_layers
+        Number of hidden layers used for encoder and decoder NNs.
+    dropout_rate
+        Dropout rate for neural networks.
+    dispersion
+        One of the following:
+
+        * ``'gene'`` - dispersion parameter of NB is constant per gene across cells
+        * ``'gene-batch'`` - dispersion can differ between different batches
+        * ``'gene-label'`` - dispersion can differ between different labels
+        * ``'gene-cell'`` - dispersion can differ for every gene in every cell
+    gene_likelihood
+        One of:
+
+        * ``'nb'`` - Negative binomial distribution
+        * ``'zinb'`` - Zero-inflated negative binomial distribution
+        * ``'poisson'`` - Poisson distribution
+    latent_distribution
+        One of:
+
+        * ``'normal'`` - Normal distribution
+        * ``'ln'`` - Logistic normal distribution (Normal(0, I) transformed by softmax)
+    **model_kwargs
+        Keyword args for :class:`~scvi.module.VAE`
+
+    Examples
+    --------
+    >>> adata = anndata.read_h5ad(path_to_anndata)
+    >>> scvi.model.SCVI.setup_anndata(adata, batch_key="batch")
+    >>> vae = scvi.model.SCVI(adata)
+    >>> vae.train()
+    >>> adata.obsm["X_scVI"] = vae.get_latent_representation()
+    >>> adata.obsm["X_normalized_scVI"] = vae.get_normalized_expression()
+
+    Notes
+    -----
+    See further usage examples in the following tutorials:
+
+    1. :doc:`/tutorials/notebooks/api_overview`
+    2. :doc:`/tutorials/notebooks/harmonization`
+    3. :doc:`/tutorials/notebooks/scarches_scvi_tools`
+    4. :doc:`/tutorials/notebooks/scvi_in_R`
+    """
+
+    _module_cls = nicheVAE
+
+    def __init__(
+        self,
+        adata: AnnData,
+        # n_cell_types: int,
+        niche_kl_weight: float = 1.0,
+        n_hidden: int = 128,
+        n_latent: int = 10,
+        n_layers: int = 1,
+        dropout_rate: float = 0.1,
+        dispersion: Literal["gene", "gene-batch", "gene-label", "gene-cell"] = "gene",
+        gene_likelihood: Literal["zinb", "nb", "poisson"] = "zinb",
+        latent_distribution: Literal["normal", "ln"] = "normal",
+        **model_kwargs,
+    ):
+        super().__init__(adata)
+
+        n_cats_per_cov = (
+            self.adata_manager.get_state_registry(
+                REGISTRY_KEYS.CAT_COVS_KEY
+            ).n_cats_per_key
+            if REGISTRY_KEYS.CAT_COVS_KEY in self.adata_manager.data_registry
+            else None
+        )
+        n_batch = self.summary_stats.n_batch
+        use_size_factor_key = (
+            REGISTRY_KEYS.SIZE_FACTOR_KEY in self.adata_manager.data_registry
+        )
+        library_log_means, library_log_vars = None, None
+        if not use_size_factor_key and self.minified_data_type is None:
+            library_log_means, library_log_vars = _init_library_size(
+                self.adata_manager, n_batch
+            )
+
+        self.module = self._module_cls(
+            n_input=self.summary_stats.n_vars,
+            # n_cell_types=n_cell_types,
+            niche_kl_weight=niche_kl_weight,
+            n_batch=n_batch,
+            n_labels=self.summary_stats.n_labels,
+            n_continuous_cov=self.summary_stats.get("n_extra_continuous_covs", 0),
+            n_cats_per_cov=n_cats_per_cov,
+            n_hidden=n_hidden,
+            n_latent=n_latent,
+            n_layers=n_layers,
+            dropout_rate=dropout_rate,
+            dispersion=dispersion,
+            gene_likelihood=gene_likelihood,
+            latent_distribution=latent_distribution,
+            use_size_factor_key=use_size_factor_key,
+            library_log_means=library_log_means,
+            library_log_vars=library_log_vars,
+            **model_kwargs,
+        )
+        self.module.minified_data_type = self.minified_data_type
+        self._model_summary_string = (
+            "SCVI Model with the following params: \nn_hidden: {}, n_latent: {}, n_layers: {}, dropout_rate: "
+            "{}, dispersion: {}, gene_likelihood: {}, latent_distribution: {}"
+        ).format(
+            n_hidden,
+            n_latent,
+            n_layers,
+            dropout_rate,
+            dispersion,
+            gene_likelihood,
+            latent_distribution,
+        )
+        self.init_params_ = self._get_init_params(locals())
+
+    def predict_neighborhood(
+        self,
+        adata: Optional[AnnData] = None,
+        indices: Optional[Sequence[int]] = None,
+        batch_size: Optional[int] = None,
+        n_samples: int = 1,
+        softmax: bool = False,
+        sigmoid: bool = False,
+    ):
+        """
+        Predict the CTs in the neighborhood of each cell.
+
+        Parameters
+        ----------
+        adata
+            AnnData object with equivalent structure to initial AnnData. If `None`, defaults to the
+            AnnData object used to initialize the model.
+        indices
+            Indices of cells in adata
+
+        """
+
+        adata = self._validate_anndata(adata)
+        self._check_if_trained()
+        scdl = self._make_data_loader(
+            adata=adata,
+            indices=indices,
+            batch_size=batch_size,
+        )
+
+        ct_pred = []
+        for tensors in scdl:
+            x = tensors[REGISTRY_KEYS.X_KEY]
+            batch = tensors[REGISTRY_KEYS.BATCH_KEY]
+
+            cont_key = REGISTRY_KEYS.CONT_COVS_KEY
+            cont_covs = tensors[cont_key] if cont_key in tensors.keys() else None
+
+            cat_key = REGISTRY_KEYS.CAT_COVS_KEY
+            cat_covs = tensors[cat_key] if cat_key in tensors.keys() else None
+
+            inference_output = self.module._regular_inference(
+                x=x,
+                batch_index=batch,
+                cont_covs=cont_covs,
+                cat_covs=cat_covs,
+                n_samples=1,
+            )
+
+            generative_inputs = self.module._get_generative_input(
+                tensors, inference_output
+            )
+
+            decoder_input = inference_output["qz"].loc
+
+            predicted_ct, _ = self.module.cell_type_decoder(decoder_input)
+
+            if predicted_ct.shape[1] > 1 and sigmoid:
+                RaiseError("Sigmoid is not supported for multi-class classification")
+
+            if sigmoid and softmax:
+                RaiseError("Sigmoid and softmax cannot be used together")
+
+            if softmax:
+                predicted_ct = F.softmax(predicted_ct, dim=-1)
+            if sigmoid:
+                predicted_ct = torch.sigmoid(predicted_ct)
+
+            ct_pred.append(predicted_ct.detach().cpu())
+
+        ct_pred = torch.cat(ct_pred).numpy()
+
+        return ct_pred
+
+    @classmethod
+    @setup_anndata_dsp.dedent
+    def setup_anndata(
+        cls,
+        adata: AnnData,
+        layer: Optional[str] = None,
+        batch_key: Optional[str] = None,
+        labels_key: Optional[str] = None,
+        size_factor_key: Optional[str] = None,
+        niche_composition_key: Optional[str] = None,
+        niche_indexes_key: Optional[str] = None,
+        sample_key: Optional[str] = None,
+        cell_coordinates_key: Optional[str] = None,
+        k_nn: int = 10,
+        latent_mean_key: Optional[str] = None,
+        latent_var_key: Optional[str] = None,
+        categorical_covariate_keys: Optional[List[str]] = None,
+        continuous_covariate_keys: Optional[List[str]] = None,
+        **kwargs,
+    ):
+        """%(summary)s.
+
+        Parameters
+        ----------
+        %(param_adata)s
+        %(param_layer)s
+        %(param_batch_key)s
+        %(param_labels_key)s
+        %(param_size_factor_key)s
+        %(param_cat_cov_keys)s
+        %(param_cont_cov_keys)s
+        """
+
+        adata.obsm[niche_indexes_key] = np.zeros((adata.n_obs, k_nn))
+
+        setup_method_args = cls._get_setup_method_args(**locals())
+        anndata_fields = [
+            LayerField(REGISTRY_KEYS.X_KEY, layer, is_count_data=True),
+            CategoricalObsField(REGISTRY_KEYS.BATCH_KEY, batch_key),
+            CategoricalObsField(REGISTRY_KEYS.LABELS_KEY, labels_key),
+            NumericalObsField(
+                REGISTRY_KEYS.SIZE_FACTOR_KEY, size_factor_key, required=False
+            ),
+            CategoricalJointObsField(
+                REGISTRY_KEYS.CAT_COVS_KEY, categorical_covariate_keys
+            ),
+            NumericalJointObsField(
+                REGISTRY_KEYS.CONT_COVS_KEY, continuous_covariate_keys
+            ),
+            ObsmField(REGISTRY_KEYS.NICHE_COMPOSITION_KEY, niche_composition_key),
+            ObsmField(REGISTRY_KEYS.Z1_mean_KEY, latent_mean_key),
+            ObsmField(REGISTRY_KEYS.Z1_var_KEY, latent_var_key),
+            ObsmField(REGISTRY_KEYS.NICHE_INDEXES_KEY, niche_indexes_key),
+        ]
+
+        # register new fields if the adata is minified
+        adata_minify_type = _get_adata_minify_type(adata)
+        if adata_minify_type is not None:
+            anndata_fields += cls._get_fields_for_adata_minification(adata_minify_type)
+        adata_manager = AnnDataManager(
+            fields=anndata_fields, setup_method_args=setup_method_args
+        )
+        adata_manager.register_fields(adata, **kwargs)
+        cls.register_manager(adata_manager)
+
+        def get_niche_indexes(
+            adata,
+            sample_key,
+            niche_indexes_key,
+            cell_coordinates_key,
+            k_nn,
+        ):
+            for sample in adata.obs[sample_key].unique():
+                sample_coord = adata.obsm[cell_coordinates_key][
+                    adata.obs[sample_key] == sample
+                ]
+
+                # Create a NearestNeighbors object
+                knn = NearestNeighbors(n_neighbors=k_nn + 1)
+
+                # Fit the kNN model to the points
+                knn.fit(sample_coord)
+
+                # Find the indices of the kNN for each point
+                _, indices = knn.kneighbors(sample_coord)
+
+                adata.obsm[niche_indexes_key][
+                    adata.obs[sample_key] == sample
+                ] = indices[:, 1:]
+            return None
+
+        get_niche_indexes(
+            adata=adata,
+            sample_key=sample_key,
+            niche_indexes_key=niche_indexes_key,
+            cell_coordinates_key=cell_coordinates_key,
+            k_nn=k_nn,
+        )
+
+    @staticmethod
+    def _get_fields_for_adata_minification(
+        minified_data_type: MinifiedDataType,
+    ) -> List[BaseAnnDataField]:
+        """Return the anndata fields required for adata minification of the given minified_data_type."""
+        if minified_data_type == ADATA_MINIFY_TYPE.LATENT_POSTERIOR:
+            fields = [
+                ObsmField(
+                    REGISTRY_KEYS.LATENT_QZM_KEY,
+                    _SCVI_LATENT_QZM,
+                ),
+                ObsmField(
+                    REGISTRY_KEYS.LATENT_QZV_KEY,
+                    _SCVI_LATENT_QZV,
+                ),
+                NumericalObsField(
+                    REGISTRY_KEYS.OBSERVED_LIB_SIZE,
+                    _SCVI_OBSERVED_LIB_SIZE,
+                ),
+            ]
+        else:
+            raise NotImplementedError(f"Unknown MinifiedDataType: {minified_data_type}")
+        fields.append(
+            StringUnsField(
+                REGISTRY_KEYS.MINIFY_TYPE_KEY,
+                _ADATA_MINIFY_TYPE_UNS_KEY,
+            ),
+        )
+        return fields
+
+    def minify_adata(
+        self,
+        minified_data_type: MinifiedDataType = ADATA_MINIFY_TYPE.LATENT_POSTERIOR,
+        use_latent_qzm_key: str = "X_latent_qzm",
+        use_latent_qzv_key: str = "X_latent_qzv",
+    ) -> None:
+        """Minifies the model's adata.
+
+        Minifies the adata, and registers new anndata fields: latent qzm, latent qzv, adata uns
+        containing minified-adata type, and library size.
+        This also sets the appropriate property on the module to indicate that the adata is minified.
+
+        Parameters
+        ----------
+        minified_data_type
+            How to minify the data. Currently only supports `latent_posterior_parameters`.
+            If minified_data_type == `latent_posterior_parameters`:
+
+            * the original count data is removed (`adata.X`, adata.raw, and any layers)
+            * the parameters of the latent representation of the original data is stored
+            * everything else is left untouched
+        use_latent_qzm_key
+            Key to use in `adata.obsm` where the latent qzm params are stored
+        use_latent_qzv_key
+            Key to use in `adata.obsm` where the latent qzv params are stored
+
+        Notes
+        -----
+        The modification is not done inplace -- instead the model is assigned a new (minified)
+        version of the adata.
+        """
+        # TODO(adamgayoso): Add support for a scenario where we want to cache the latent posterior
+        # without removing the original counts.
+        if minified_data_type != ADATA_MINIFY_TYPE.LATENT_POSTERIOR:
+            raise NotImplementedError(f"Unknown MinifiedDataType: {minified_data_type}")
+
+        if self.module.use_observed_lib_size is False:
+            raise ValueError(
+                "Cannot minify the data if `use_observed_lib_size` is False"
+            )
+
+        minified_adata = get_minified_adata_scrna(self.adata, minified_data_type)
+        minified_adata.obsm[_SCVI_LATENT_QZM] = self.adata.obsm[use_latent_qzm_key]
+        minified_adata.obsm[_SCVI_LATENT_QZV] = self.adata.obsm[use_latent_qzv_key]
+        counts = self.adata_manager.get_from_registry(REGISTRY_KEYS.X_KEY)
+        minified_adata.obs[_SCVI_OBSERVED_LIB_SIZE] = np.squeeze(
+            np.asarray(counts.sum(axis=1))
+        )
+        self._update_adata_and_manager_post_minification(
+            minified_adata, minified_data_type
+        )
+        self.module.minified_data_type = minified_data_type
